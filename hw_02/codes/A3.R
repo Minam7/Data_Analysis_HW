54a7c9b17ff5f5064500303545bac969000f8799
mina <mina.m777@yahoo.com>
2018-02-19 18:16:44 +0330

    hw2 update

--- hw_02/codes/A3.R
+++ hw_02/codes/A3.R
@@ -12,3 +12,11 @@ lte_price = mobile %>% group_by(LTE) %>% summarise(average= mean(price, na.rm = 
 
 p_lte = ggplot(data = lte_price, aes(y = average, x = LTE, fill = average)) + geom_bar(stat="identity") + ggtitle('average price based on LTE support') + xlab('LTE support') + ylab('average price') + guides(fill=guide_legend(title="price"))
 p_lte
+
+mobile = mobile %>% mutate(sim_lte = interaction(sim_no, LTE))
+View(mobile)
+
+mobile_lte_price = mobile %>% group_by(sim_lte) %>% summarise(average= mean(price, na.rm = TRUE))
+
+p_lte_price = ggplot(data = mobile_lte_price, aes(y = average, x = sim_lte, fill = average)) + geom_bar(stat="identity") + ggtitle('average price based on number of simcards and having LTE') + xlab('having LTE and number of simcards') + ylab('average price') + guides(fill=guide_legend(title="price"))
+p_lte_price
