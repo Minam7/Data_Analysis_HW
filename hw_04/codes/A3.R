37c6b549fd8e31e27066ae0f4c2e3bb71a92bf82
mina <mina.m777@yahoo.com>
2018-03-08 13:53:02 +0330

    hw4 3,5

--- hw_04/codes/A3.R
+++ hw_04/codes/A3.R
@@ -2,8 +2,7 @@ edu_resources <- bsg %>% select(idstud, idcntry, std_rsc = bsdg06s, contains("bs
   mutate(score = rowMeans(.[, 4:13])) %>% 
   select(idstud, idcntry, std_rsc, edu_rsc, score) %>% 
   mutate(study_resource = ifelse(std_rsc < 1, "No Resource", "With Resource")) %>% 
-  mutate(edu_resource = ifelse(edu_rsc > 2, "No Resource", "With Resource")) %>% 
-  select(idstud, idcntry, study_resource, edu_resource, score) %>% 
+  select(idstud, idcntry, study_resource, edu_resource=edu_rsc, score) %>% 
   filter(!is.na(study_resource)) %>% 
   filter(!is.na(edu_resource))
 
@@ -28,22 +27,26 @@ hchart(density(no_resource$score), type = "area", name=list("No Resource")) %>%
 
 t.test(no_resource$score, with_resource$score, alt = "less")
 
-# other data
-edu_resources %>% ggplot(mapping = aes(edu_resource, score, fill = edu_resource)) +
+# other data with anova
+edu_resources <- edu_resources %>% mutate(edu_stuff = ifelse(edu_resource == 1, "Many Resources", ifelse(edu_resource == 2, "Some Resources", "Few Resources")))
+edu_resources %>% ggplot(mapping = aes(edu_stuff, score, fill = edu_stuff)) +
   geom_boxplot(notch=FALSE) +
   ylab("score") +
   xlab("resource") +
   ggtitle("Density of score based on home resources") +
-  guides(fill=guide_legend(title="home resources"))
+  guides(fill=guide_legend(title="home resources")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-no_resource <- edu_resources %>% filter(edu_resource == "No Resource")
-with_resource <- edu_resources %>% filter(edu_resource == "With Resource")
+few_resource <- edu_resources %>% filter(edu_resource == 3)
+some_resource <- edu_resources %>% filter(edu_resource == 2)
+many_resource <- edu_resources %>% filter(edu_resource == 1)
 
-hchart(density(no_resource$score), type = "area", name=list("No Resource")) %>%
-  hc_add_series(density(with_resource$score), type = "area", name=list("With Resource")) %>% 
+hchart(density(few_resource$score), name=list("Few Resources")) %>%
+  hc_add_series(density(some_resource$score), name=list("Some Resources")) %>% 
+  hc_add_series(density(many_resource$score), name=list("Many Resources")) %>% 
   hc_add_theme(hc_theme_google()) %>% 
   hc_yAxis(title = list(text = "density")) %>% 
   hc_xAxis(title = list(text = "score")) %>% 
   hc_title(text = "Density of score based on home resources", style = list(fontWeight = "bold"))
 
-t.test(score ~ edu_resource, data = edu_resources, alt = "less")
+summary(aov(score ~ edu_resource, data = edu_resources))
---
+++ hw_04/codes/A5.R
@@ -0,0 +1,52 @@
+tchr_std_perf <- bst %>% select(country= idcntry, teacher= idteach, student= idstud, contains("bsssci"), contains("bsmmat"), school= idschool) %>% 
+  mutate(score = rowMeans(.[, 4:13])) %>% 
+  select(country, teacher, student, school, score) %>% 
+  filter(!is.na(country) & !is.na(teacher) & !is.na(student) & !is.na(school) & !is.na(score))
+
+tchr_bg <- btg %>% select(country= idcntry, teacher= idteach, school= idschool, edu= btbg04) %>% 
+  filter(!is.na(country) & !is.na(teacher) & !is.na(school) & !is.na(edu))
+
+tchr_bg_std_perf <- tchr_std_perf %>% inner_join(tchr_bg, by= c("country", "teacher", "school"))
+
+group1 <- tchr_bg_std_perf %>% filter(edu == 1)
+group2 <- tchr_bg_std_perf %>% filter(edu == 2)
+group3 <- tchr_bg_std_perf %>% filter(edu == 3)
+group4 <- tchr_bg_std_perf %>% filter(edu == 4)
+group5 <- tchr_bg_std_perf %>% filter(edu == 5)
+group6 <- tchr_bg_std_perf %>% filter(edu == 6)
+group7 <- tchr_bg_std_perf %>% filter(edu == 7)
+
+tchr_bg_std_perf <- tchr_bg_std_perf %>% 
+  mutate(education = ifelse(edu == 1, "Did not complete Upper secondary",
+                ifelse(edu == 2, "Upper secondary",
+                       ifelse(edu == 3, "Post-secondary, non-tertiary",
+                              ifelse(edu == 4, "Short-cycle tertiary",
+                                     ifelse(edu == 5, "Bachelor’s or equivalent",
+                                            ifelse(edu == 6, "Master’s or equivalent",
+                                                   ifelse(edu == 7, "Doctor or equivalent", NA)))))))) %>% 
+  select(country, teacher, student, school, score, education) %>% 
+  filter(!is.na(education))
+
+tchr_bg_std_perf %>% ggplot(mapping = aes(education, score, fill = education)) +
+  geom_boxplot(notch=FALSE) +
+  ylab("score") +
+  xlab("teacher education") +
+  ggtitle("Density of score based on teacher education") +
+  guides(fill=guide_legend(title="teacher education")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+hchart(density(group1$score), name=list("Did not complete Upper secondary")) %>%
+  hc_add_series(density(group2$score), name=list("Upper secondary")) %>% 
+  hc_add_series(density(group3$score), name=list("Post-secondary, non-tertiary")) %>% 
+  hc_add_series(density(group4$score), name=list("Short-cycle tertiary")) %>% 
+  hc_add_series(density(group5$score), name=list("Bachelor’s or equivalent")) %>% 
+  hc_add_series(density(group6$score), name=list("Master’s or equivalent")) %>% 
+  hc_add_series(density(group7$score), name=list("Doctor or equivalent")) %>% 
+  hc_add_theme(hc_theme_google()) %>% 
+  hc_yAxis(title = list(text = "density")) %>% 
+  hc_xAxis(title = list(text = "score")) %>% 
+  hc_title(text = "Density of score based on teacher education", style = list(fontWeight = "bold"))
+
+summary(aov(score ~ education, data = tchr_bg_std_perf))
+
