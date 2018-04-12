03b3010701999b097cba65f6d29119de443f8d92
mina <mina.m777@yahoo.com>
2018-04-13 00:22:30 +0430

    update A8, A7

--- hw_06/codes/A7.R
+++ hw_06/codes/A7.R
@@ -5,3 +5,9 @@ plot(new_reg_model)
 par(mfrow=c(1,1))
 
 cor.test(house_numeric$SalePrice, residuals(new_reg_model))
+
+
+data = data.frame(house_numeric$SalePrice, abs(residuals(new_reg_model)))
+data_matrix <- data %>% .[,-1] %>% as.matrix() 
+
+chisq.test(data_matrix)
