a4985dc04aaf6b7a0bbe0c68036ebb3063146bfd
mina <mina.m777@yahoo.com>
2018-04-12 20:44:23 +0430

    A1

---
+++ hw_06/codes/A1.R
@@ -0,0 +1,13 @@
+library(knitr)
+library(corrplot)
+library(psych)
+
+house_numeric <- house %>% select_if(is.numeric)
+all_cors = cor(house_numeric)
+knitr::kable(all_cors)
+
+corrplot(all_cors, method = "color", tl.cex = 0.75/par("cex"), cl.cex = 0.75/par("cex"))
+
+knitr::kable(sort(corr.test(all_cors, adjust = "none", alpha = 0.05)$p[38,], decreasing = TRUE))
+
+knitr::kable(sort(abs(all_cors['SalePrice',]), decreasing = TRUE)[2:11])
