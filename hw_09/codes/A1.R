2c96aa14a5a639417caa3617ccea292672a282db
mina <mina.m777@yahoo.com>
2018-05-20 00:44:52 +0430

    HW9 A1

--- hw_09/codes/A1.R
+++ hw_09/codes/A1.R
@@ -15,13 +15,57 @@ knitr::kable(stock_yearly %>% group_by(Sector) %>% arrange(desc(profit)) %>% sli
 
 stock_yearly %>% group_by(Sector) %>% arrange(desc(profit)) %>% slice(1) %>% 
   hchart(type = "column", hcaes(x = stock_name, y = profit, group = Sector)) %>% 
-  hc_yAxis(title = list(text = "Profit")) %>% 
+  hc_yAxis(title = list(text = "Profit Percentage")) %>% 
   hc_xAxis(type = 'category', title = list(categories = stock_yearly$stock_name, text = "Stock Name")) %>% 
   hc_title(text = "Top Annual Profitable Stocks", style = list(fontWeight = "bold")) %>% 
   hc_add_theme(hc_theme_flat())
 
+
 # Every Two Year
-# TODO
+stock_yearly$two_year_profit = 0
+
+for (i in name) {
+  stock = stock_yearly %>% filter(stock_name == i)
+  for (j in 1:nrow(stock)) {
+    if (j != 1) {
+      # profit for other years!
+      stock_yearly[stock_yearly$stock_name == i & stock_yearly$Year == stock[j,]$Year,]$two_year_profit = 100*((stock[j,]$End - stock[j - 1,]$Start)/stock[j - 1,]$Start)
+    }
+  }
+}
+
+knitr::kable(stock_yearly %>% arrange(desc(two_year_profit)) %>% slice(1))
+
+knitr::kable(stock_yearly %>% group_by(Sector) %>% arrange(desc(two_year_profit)) %>% slice(1))
+
+stock_yearly %>% group_by(Sector) %>% arrange(desc(two_year_profit)) %>% slice(1) %>% 
+  hchart(type = "column", hcaes(x = stock_name, y = two_year_profit, group = Sector)) %>% 
+  hc_yAxis(title = list(text = "Profit Percentage")) %>% 
+  hc_xAxis(type = 'category', title = list(categories = stock_yearly$stock_name, text = "Stock Name")) %>% 
+  hc_title(text = "Top Two Year Profitable Stocks", style = list(fontWeight = "bold")) %>% 
+  hc_add_theme(hc_theme_538())
+
 
 # Every Five Year
-# TODO
+stock_yearly$five_year_profit = 0
+
+for (i in name) {
+  stock = stock_yearly %>% filter(stock_name == i)
+  for (j in 1:nrow(stock)) {
+    if (j > 4) {
+      # profit for other years!
+      stock_yearly[stock_yearly$stock_name == i & stock_yearly$Year == stock[j,]$Year,]$five_year_profit = 100*((stock[j,]$End - stock[j - 4,]$Start)/stock[j - 4,]$Start)
+    }
+  }
+}
+
+knitr::kable(stock_yearly %>% arrange(desc(five_year_profit)) %>% slice(1))
+
+knitr::kable(stock_yearly %>% group_by(Sector) %>% arrange(desc(five_year_profit)) %>% slice(1))
+
+stock_yearly %>% group_by(Sector) %>% arrange(desc(five_year_profit)) %>% slice(1) %>% 
+  hchart(type = "column", hcaes(x = stock_name, y = five_year_profit, group = Sector)) %>% 
+  hc_yAxis(title = list(text = "Profit Percentage")) %>% 
+  hc_xAxis(type = 'category', title = list(categories = stock_yearly$stock_name, text = "Stock Name")) %>% 
+  hc_title(text = "Top Five Year Profitable Stocks", style = list(fontWeight = "bold")) %>% 
+  hc_add_theme(hc_theme_elementary())
