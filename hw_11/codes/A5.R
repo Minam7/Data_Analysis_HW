0e6384f030b2c9f2d18a2197b96d44d13cb6a7bf
mina <mina.m777@yahoo.com>
2018-06-09 19:18:21 +0430

    hw11 A5

--- hw_11/codes/A5.R
+++ hw_11/codes/A5.R
@@ -1,4 +1,5 @@
 library(rworldmap)
+library(countrycode)
 
 disaster_sum <- read_delim("data/disaster.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>% 
   rename(lat = LATITUDE,long = LONGITUDE, magnit = INTENSITY,country = COUNTRY,year = YEAR, death=TOTAL_DEATHS) %>% 
@@ -7,5 +8,21 @@ disaster_sum <- read_delim("data/disaster.txt", "\t", escape_double = FALSE, tri
 disaster_sum = na.omit(disaster_sum)
 disaster_sum <- disaster_sum %>% group_by(country) %>% summarise(fatality_sum = sum(death), fatality_mean = mean(death))
 
+disaster_sum$country <- countrycode(disaster_sum$country, "country.name", "iso3c", warn = TRUE, nomatch = NA,
+                                    custom_dict = NULL, custom_match = NULL, origin_regex = FALSE)
+
 heatMap <- joinCountryData2Map(disaster_sum, joinCode = "ISO3",
                               nameJoinColumn = "country")
+
+mapCountryData(heatMap, nameColumnToPlot = "fatality_sum", 
+               mapTitle="Earthquakes total fatality", oceanCol=gray(0.3),
+               colourPalette=c("cadetblue1","cadetblue3", "deepskyblue2", "deepskyblue3", "deepskyblue4", "dodgerblue4", "navy"), missingCountryCol = "white")
+
+
+heatMap <- joinCountryData2Map(disaster_sum, joinCode = "ISO3",
+                               nameJoinColumn = "country")
+
+mapCountryData(heatMap, nameColumnToPlot = "fatality_mean", 
+               mapTitle="Earthquakes fatality rate", oceanCol=gray(0.3),
+               colourPalette=c("cadetblue1","cadetblue3", "deepskyblue2", "deepskyblue3", "deepskyblue4", "dodgerblue4", "navy"), missingCountryCol = "white")
+
