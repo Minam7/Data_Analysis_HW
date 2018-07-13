4e790004a4e648fb6959907bcb17e4afd09dd7dd
mina <mina.m777@yahoo.com>
2018-07-14 01:37:01 +0430

    update hw11 q4

--- hw_11/codes/A4.R
+++ hw_11/codes/A4.R
@@ -1,31 +1,13 @@
-iran_sig_eq <- iran_eq %>% filter(Mag >= 7) %>% arrange(desc(OriginTime))
+iran_sig_eq <- read_delim("data/disaster.txt", "\t", escape_double = FALSE, trim_ws = TRUE) %>% 
+  rename(lat = LATITUDE,long = LONGITUDE, magnit = INTENSITY,name = COUNTRY,year = YEAR) %>% 
+  dplyr::select(lat, long, magnit, name, year) %>% filter(magnit >= 7 & name == 'IRAN') %>% 
+  mutate(diff = year - lag(year))
 
-last <- iran_sig_eq %>% slice(1)
-diff = as.numeric(Sys.time() -last$OriginTime, units="days")
-diff = diff %/% 365
+cdf_iran <- ecdf(iran_sig_eq$diff)
+summary(cdf_iran)
 
-diff_year = diff + 5
-after_year = 1 # for first one
-for (i in 1:nrow(iran_sig_eq)) {
-  if (i > 1) {
-    differ = as.numeric(iran_sig_eq[i-1,]$OriginTime - iran_sig_eq[i,]$OriginTime, units="days") %/% 365
-    if (differ >= diff_year) {
-      after_year = after_year + 1
-    }
-  }
-}
-after_year = after_year / nrow(iran_sig_eq)
+B = 1 - cdf_iran(1)
+AB = cdf_iran(6)
+p = (AB)/(B)
 
-after_two = 1 # for first one
-for (i in 1:nrow(iran_sig_eq)) {
-  if (i > 1) {
-    differ = as.numeric(iran_sig_eq[i-1,]$OriginTime - iran_sig_eq[i,]$OriginTime, units="days") %/% 365
-    if (differ >= 2) {
-      after_two = after_two + 1
-    }
-  }
-}
-after_two = after_two / nrow(iran_sig_eq)
-
-p = (after_year)/(after_two)
 cat("Probability of earthquake in 5 years is", p)
