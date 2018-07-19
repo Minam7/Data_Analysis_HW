98c9264bac9e7c7fc59bb9898cbdbdb318e08d65
mina <mina.m777@yahoo.com>
2018-07-20 02:56:07 +0430

    hw12 A2c

--- hw_12/codes/A2.R
+++ hw_12/codes/A2.R
@@ -18,3 +18,41 @@ library(corrplot)
 corrplot(cor(movie_genre  %>% select(c(7:24))), method = "color", tl.cex = 0.5/par("cex"), cl.cex = 0.5/par("cex"))
 
 # c: average rating for genres
+library(ggplot2)
+
+rate_genre <- movie_genre %>% 
+  mutate(Action = Rate*Action,
+         Adventure = Rate*Adventure,
+         Animation = Rate*Animation,
+         Children = Rate*Children,
+         Comedy = Rate*Comedy,
+         Crime = Rate*Crime,
+         Documentary = Rate*Documentary,
+         Drama = Rate*Drama ,
+         Fantasy = Rate*Fantasy,
+         `Film-Noir` = Rate*`Film-Noir`,
+         Horror = Rate*Horror,
+         Musical = Rate*Musical,
+         Mystery = Rate*Mystery,
+         Romance = Rate*Romance,
+         `Sci-Fi` = Rate*`Sci-Fi`,
+         Thriller = Rate*Thriller,
+         War = Rate*War,
+         Western = Rate*Western)
+
+rate_sum_genre <- rate_genre %>% summarise_at(c(7:24), sum)
+count_genre <- movie_genre  %>% summarise_at(c(7:24), sum)
+
+rate_mean_genre <- rate_sum_genre/count_genre
+mrate_mean_genre = melt(rate_mean_genre)
+colnames(mrate_mean_genre) = c('genre', 'mean')
+
+p = ggplot(data = mrate_mean_genre, mapping = aes(x = genre, y = mean, fill = mean)) + 
+  ggtitle("Genre Average Rating") + geom_bar(stat="identity") + 
+  scale_fill_gradient(low="gold", high="darkgreen") + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+p
+
+knitr::kable(mrate_mean_genre)
+
+# d: cinema golden age
